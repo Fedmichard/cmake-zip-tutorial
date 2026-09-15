5d9701901a0fac3e8af0f6e137b09610f947afc8
Fedmichard <francoisfedmichard@gmail.com>
2026-09-14 21:56:56 -0400

    Finishing working build system generator, will default to Visual Studio Build system and specify Ninja manually.

--- Step0/HelloWorld.cxx
+++ Step0/HelloWorld.cxx
@@ -4,3 +4,11 @@ int main()
 {
   std::printf("Hello World\n");
 }
+
+/*
+ * cmake -S <dir>: specifies the specific path for the project root directory
+ * cmake -B <dir>: specifies the specific path for the build directory, where the build and all the artifacts of the build will live (defaults to working dir)
+ * cmake --build <dir>: Runs the build system inside the build directory
+ * cmake -G <build system> -B build: generates build inside root/build using specifies build system
+ * If we use multiple generators, we must specify different build directories or clear the previous one
+*/
