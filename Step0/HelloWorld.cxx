73032cff91dfe6de2a49d8f9d69c2fad029ec6ef
Fedmichard <francoisfedmichard@gmail.com>
2026-09-16 22:47:03 -0400

    finalize

--- Step0/HelloWorld.cxx
+++ Step0/HelloWorld.cxx
@@ -12,46 +12,4 @@ int main()
  * cmake -G <build system> -B build: generates build inside root/build using specifies build system
  * If we use multiple generators, we must specify different build directories or clear the previous one
  * For some reason for me it's defaulting at 
-*/#include <cstdio>
-
-int main()
-{
-  std::printf("Hello World\n");
-}
-
-/*
- * cmake -S <dir>: specifies the specific path for the project root directory
- * cmake -B <dir>: specifies the specific path for the build directory, where the build and all the artifacts of the build will live (defaults to working dir)
- * cmake --build <dir>: Runs the build system inside the build directory
- * cmake -G <build system> -B build: generates build inside root/build using specifies build system
- * If we use multiple generators, we must specify different build directories or clear the previous one
- * For some reason for me it's defaulting at 
-*/#include <cstdio>
-
-int main()
-{
-  std::printf("Hello World\n");
-}
-
-/*
- * cmake -S <dir>: specifies the specific path for the project root directory
- * cmake -B <dir>: specifies the specific path for the build directory, where the build and all the artifacts of the build will live (defaults to working dir)
- * cmake --build <dir>: Runs the build system inside the build directory
- * cmake -G <build system> -B build: generates build inside root/build using specifies build system
- * If we use multiple generators, we must specify different build directories or clear the previous one
- * For some reason for me it's defaulting at 
-*/#include <cstdio>
-
-int main()
-{
-  std::printf("Hello World\n");
-}
-
-/*
- * cmake -S <dir>: specifies the specific path for the project root directory
- * cmake -B <dir>: specifies the specific path for the build directory, where the build and all the artifacts of the build will live (defaults to working dir)
- * cmake --build <dir>: Runs the build system inside the build directory
- * cmake -G <build system> -B build: generates build inside root/build using specifies build system
- * If we use multiple generators, we must specify different build directories or clear the previous one
- * For some reason for me it's defaulting at 
 */
